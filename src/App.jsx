ab895be183476978d6ec17301cb616ee7d97f793
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-15 14:09:15 +0100

    add logout in nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,39 @@ import Dashboard from "./pages/Dashboard";
 import Program from "./pages/Program";
 import Goal from "./pages/Goal";
 import Workout from "./pages/Workout";
+import keycloak from "./keycloak";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <main className="container">
-        <Routes>
-        <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route path="/goal" element={<Goal />} />
+      {keycloak.authenticated ? (
+        <>
+          <Navbar />
+          <main className="container">
+            <Routes>
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route path="/goal" element={<Goal />} />
 
-          <Route path="/program" element={<Program />} />
+              <Route path="/program" element={<Program />} />
 
-          <Route path="/workout" element={<Workout />} />
-          <Route path="/" element={<StartPage />} />
-          <Route
-            path="/profile"
-            element={
-              <KeycloakRoute role={ ROLES.User }>
-                <ProfilePage />
-              </KeycloakRoute>
-            }
-          />
-        </Routes>
-      </main>
+              <Route path="/workout" element={<Workout />} />
+              <Route
+                path="/profile"
+                element={
+                  <KeycloakRoute role={ROLES.User}>
+                    <ProfilePage />
+                  </KeycloakRoute>
+                }
+              />
+            </Routes>
+          </main>
+        </> 
+      ) : (
+
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+      </Routes>
+      )}
     </BrowserRouter>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react'
 import { Link } from "react-router-dom";
 import keycloak from "../../keycloak";
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '/dashboard', current: true },
@@ -67,6 +67,18 @@ function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              
+       
+              {keycloak.authenticated && (
+              <button
+                onClick={() => keycloak.logout()}
+                  type="button"
+                  className="bg-gray-800 p-1 text-gray-400 hover:text-white"
+                >
+                  <span className="sr-only">Logout</span>
+                  <ArrowLeftOnRectangleIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+                )}
                 <button
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -87,48 +99,7 @@ function Navbar() {
                       />
                     </Menu.Button>
                   </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
+                 
                 </Menu>
               </div>
             </div>
