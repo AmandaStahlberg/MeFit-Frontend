b7ccb33302d0127c6dfd5ff8401035ad47a44780
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-03-17 12:45:37 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,39 +5,52 @@ import TimeAndDate from "../components/timeAndDate/TimeAndDate";
 import keycloak from "../keycloak";
 
 export default function Dashboard() {
-  return (
-    <div className="h-screen">
-      <header className="">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          {keycloak.tokenParsed && (
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Welcome, &nbsp;
-              {keycloak.tokenParsed.name}
-            </h1>
-          )}
+    const id = 1;
+
+    fetch(`http://localhost:8080/api/v1/users/${id}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${keycloak.token}`,
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error(error));
+
+    return (
+        <div className="h-screen">
+            <header className="">
+                <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                    {keycloak.tokenParsed && (
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                            Welcome, &nbsp;
+                            {keycloak.tokenParsed.name}
+                        </h1>
+                    )}
+                </div>
+            </header>
+            <main className="h-3/4">
+                <div className="grid grid-cols-3 gap-6 h-full">
+                    <div className="col-span-2 bg-white shadow-md border border-stone-100 text-center p-4">
+                        <h1 className="text-xl font-bold tracking-tight text-gray-900">
+                            Training programs
+                        </h1>
+                        <Programs />
+                    </div>
+                    <div className="col-span-1 grid grid-rows-3 gap-6">
+                        <div className="grid row-span-1 bg-white shadow-md border border-stone-100 place-content-center">
+                            <TimeAndDate />
+                        </div>
+                        <div className="row-span-2 bg-white shadow-md border border-stone-100 text-center p-4">
+                            <h1 className="text-xl font-bold tracking-tight text-gray-900">
+                                Goals
+                            </h1>
+                            <Goals />
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
-      </header>
-      <main className="h-3/4">
-        <div className="grid grid-cols-3 gap-6 h-full">
-          <div className="col-span-2 bg-white shadow-md border border-stone-100 text-center p-4">
-            <h1 className="text-xl font-bold tracking-tight text-gray-900">
-              Training programs
-            </h1>
-            <Programs />
-          </div>
-          <div className="col-span-1 grid grid-rows-3 gap-6">
-            <div className="grid row-span-1 bg-white shadow-md border border-stone-100 place-content-center">
-              <TimeAndDate />
-            </div>
-            <div className="row-span-2 bg-white shadow-md border border-stone-100 text-center p-4">
-              <h1 className="text-xl font-bold tracking-tight text-gray-900">
-                Goals
-              </h1>
-              <Goals />
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
