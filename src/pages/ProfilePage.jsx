d2edd61b252350d7434e170db3c534d3fc383997
IsabelleTancred <isabelleetancred@gmail.com>
2023-03-20 23:57:44 +0100

    Feat: uppdate profile in db. not working

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -12,37 +12,68 @@ function ProfilePage() {
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState();
   const [medicalConditions, setMedicalConditions] = useState();
-  const [disabilities, setDisabilities] = useState ();
+  const [disabilities, setDisabilities] = useState();
 
-  const stored = { username, name, height, weight, medicalConditions, disabilities };
-
-  
+  const stored = {
+    username,
+    name,
+    height,
+    weight,
+    medicalConditions,
+    disabilities,
+  };
 
   function handleEditComplete(result) {
     if (result != null) {
-      /*const response =  fetch(`http://localhost:8080/api/v1/profile/`, {
+      const response = fetch(`http://localhost:8080/api/v1/user/${201901239}`, {
+        //TODO byt ut mot user.id
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${keycloak.token}`,
+        },
+        body: JSON.stringify({
+          username: result.username, //TODO: borde på något sätt ändras/uppdateras i keycloak också
+          name: result.name,
+        }),
+      });
+
+      if (response.ok) {
+        const response2 = fetch(
+          `http://localhost:8080/api/v1/profile/${201901239}`,
+          {
+            //TODO byt ut mot user.id
             method: "PUT",
             headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${keycloak.token}`,
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${keycloak.token}`,
             },
-            body: JSON.stringify( result ),
-        });
-      if (response.ok){ */ 
-      setUsername(result.username);
-      setName(result.name);
-      setHeight(result.height);
-      setWeight(result.weight);
-      setMedicalConditions(result.medicalConditions);
-      setDisabilities(result.disabilities);
-     // }
+            body: JSON.stringify({
+              height: result.height,
+              weight: result.weight,
+              medicalConditions: result.medicalConditions,
+              disabilities: result.disabilities,
+            }),
+          }
+        );
+        if (response2.ok) {
+          setUsername(result.username);
+          setName(result.name);
+          setHeight(result.height);
+          setWeight(result.weight);
+          setMedicalConditions(result.medicalConditions);
+          setDisabilities(result.disabilities);
+        }
+      }
+      setEditMode(false);
     }
-    setEditMode(false);
   }
 
   return (
     <div className="h-screen">
-      <h1 className="text-3xl font-bold tracking-tight text-gray-900">My Profile</h1>
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+        My Profile
+      </h1>
       <div>
         {editMode ? (
           <>
@@ -63,9 +94,8 @@ function ProfilePage() {
               <div>
                 <h4>Token</h4>
                 <pre>{keycloak.token}</pre>
-                <p>Sub: { keycloak.tokenParsed.sub }</p>
+                <p>Sub: {keycloak.tokenParsed.sub}</p>
               </div>
-
             )}
           </>
         )}
