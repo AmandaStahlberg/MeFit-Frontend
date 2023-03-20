b2996b76b1a31498e3d951b12424db153fb5cdbd
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-20 16:26:27 +0100

    get programs from db

--- src/components/programs/Programs.jsx
+++ src/components/programs/Programs.jsx
@@ -1,14 +1,41 @@
-import React from "react";
-
-const dummyData = [
-  { name: "Biggest Arms, look like Johnny Bravo", category: "Upper body" },
-  { name: "No chicken legs here, mate", category: "Lower body" },
-];
+import React, { useEffect, useState } from "react";
+import keycloak from "../../keycloak";
 
 function Programs() {
+  const [programs, setPrograms] = useState([]);
+  const [programsFetched, setProgramsFetched] = useState(false);
+
+  useEffect(() => {
+    if (!programsFetched) {
+      fetchPrograms();
+    }
+  }, [programsFetched]);
+
+  const fetchPrograms = () => {
+    fetch(`http://localhost:8080/api/v1/programs`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${keycloak.token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          // handleNoUser();
+          throw new Error("Response not OK");
+        }
+      })
+      .then((data) => {
+        setPrograms(data);
+        setProgramsFetched(true);
+      })
+      .catch((error) => console.error(error));
+  };
   return (
     <ul>
-      {dummyData.map((program, key) => (
+      {programs.map((program, key) => (
         <li
           key={key}
           className="flex justify-between focus:bg-gray-900 focus:text-white text-gray-700 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
