6202a8a3b9025de5e56bb6c79e994729eca6e482
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-21 10:36:51 +0100

    get users on userspage

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import keycloak from "../../keycloak";
 
 const dummyUsers = [
   {
@@ -29,6 +30,38 @@ const dummyUsers = [
 ];
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [usersFetched, setUsersFetched] = useState(false);
+
+  useEffect(() => {
+    if (!usersFetched) {
+      fetchPrograms();
+    }
+  }, [usersFetched]);
+
+  const fetchPrograms = () => {
+    fetch(`http://localhost:8080/api/v1/users`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${keycloak.token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          // handleNoUser();
+          throw new Error("Response not OK");
+        }
+      })
+      .then((data) => {
+        setUsers(data);
+        setUsersFetched(true);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <>
       <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
@@ -36,10 +69,10 @@ function Users() {
           <thead className=" bg-gray-50">
             <tr className="md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
               <th className="px-6 py-4 font-medium text-gray-900 text-left block md:table-cell">
-                Name
+                Username
               </th>
               <th className="px-6 py-4 text-gray-900 text-left font-medium block md:table-cell">
-                User Name
+                Name
               </th>
               <th className="px-6 py-4 text-gray-900 text-left font-medium block md:table-cell">
                 Email Address
@@ -50,7 +83,7 @@ function Users() {
               <th className="px-6 py-4 text-gray-900 text-left font-medium block md:table-cell"></th>
             </tr>
           </thead>
-          {dummyUsers.map((user, index) => (
+          {users.map((user, index) => (
             <tbody
               key={index}
               className="block md:table-row-group divide-y divide-gray-100 border-t border-gray-100"
@@ -58,27 +91,30 @@ function Users() {
               <tr className="md:border-none block md:table-row hover:bg-gray-50">
                 <td className="p-2 text-left block md:table-cell md:px-6 md:py-4 font-normal text-gray-900">
                   <span className="inline-block w-1/3 md:hidden font-bold">
-                    Name
+                    Username
                   </span>
-                  {user.Name}
+                  {user.username}
                 </td>
                 <td className="p-2 text-left block md:table-cell md:px-6 md:py-4 font-normal text-gray-900">
                   <span className="inline-block w-1/3 md:hidden font-bold">
-                    User Name
+                    Name
                   </span>
-                  {user.UserName}
                 </td>
                 <td className="p-2 text-left block md:table-cell md:px-6 md:py-4 font-normal text-gray-900">
                   <span className="inline-block w-1/3 md:hidden font-bold">
                     Email Address
                   </span>
-                  {user.Email}
+                  {console.log(user)}
                 </td>
                 <td className="p-2 text-left block md:table-cell md:px-6 md:py-4 font-normal text-gray-900">
                   <span className="inline-block w-1/3 md:hidden font-bold">
                     Role
                   </span>
-                  {user.Role}
+                  {user.isAdmin === true
+                    ? "Admin"
+                    : user.isContributor === true
+                    ? "Contributor"
+                    : "User"}
                 </td>
                 <td className="p-2 md:text-right block md:table-cell md:px-6 md:py-4 font-normal text-gray-900">
                   <span className="inline-block w-1/3 md:hidden font-bold">
