621959aaedf1d6ee0fa53ec329b3c5447f624ad1
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-22 09:56:46 +0100

    fetch workouts and exercises in modal

--- src/components/modals/ProgramsModal.jsx
+++ src/components/modals/ProgramsModal.jsx
@@ -1,32 +1,52 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useRef, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import keycloak from "../../keycloak";
 
 export default function ProgramsModal() {
   const [open, setOpen] = useState(false);
-
   const cancelButtonRef = useRef(null);
 
-  const dummyData = [
-    { name: "Biceps", type: "Strength" },
-    { name: "Triceps", type: "Strength" },
-    { name: "Condition", type: "Cardio" },
-    { name: "Lower legs", type: "Strength" },
-  ];
+  const [workouts, setWorkouts] = useState([]);
+  const [workoutsFetched, setWorkoutsFetched] = useState(false);
 
-  const dummyData2 = [
-    {
-      name: "Push up",
-      description:
-        "Pushups are done in prone position with palms under shoulders, balls of feet on the ground. The body is pushed up and down with arms straightening and bending alternately, while keeping the back straight.",
-    },
-    {
-      name: "Chins",
-      description: "Hang in a bar and drag your self up and down.",
-    },
-    { name: "Bicep curls", description: "Curl a dumbbell up and down" },
-    { name: "Spinning", description: "Pedal spinning bike" },
-  ];
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { name, category, workout } = e.target.elements;
+    console.log({
+      name: name.value,
+      category: category.value,
+      workout: workout.value,
+    });
+  }
 
+  useEffect(() => {
+    if (!workoutsFetched) {
+      fetchWorkouts();
+    }
+  }, [workoutsFetched]);
+
+  const fetchWorkouts = () => {
+    fetch(`http://localhost:8080/api/v1/workouts`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${keycloak.token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          // handleNoUser();
+          throw new Error("Response not OK");
+        }
+      })
+      .then((data) => {
+        setWorkouts(data);
+        setWorkoutsFetched(true);
+      })
+      .catch((error) => console.error(error));
+  };
   return (
     <>
       <button
@@ -56,29 +76,59 @@ export default function ProgramsModal() {
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="">
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <Dialog.Title
-                          as="h3"
-                          className="text-base font-semibold leading-6 text-gray-900"
-                        >
-                          Add new training program
-                        </Dialog.Title>
-                        <div className="mt-2 w-full max-w-xs">
-                          <form className="w-full">
+          <form className="w-full" onSubmit={handleSubmit}>
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div className="">
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <Dialog.Title
+                            as="h3"
+                            className="text-base font-semibold leading-6 text-gray-900"
+                          >
+                            Add new training program
+                          </Dialog.Title>
+                          <div className="mt-2 w-full max-w-xs">
+                            <div className="mb-6">
+                              <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="name"
+                              >
+                                Name
+                              </label>
+                              <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="name"
+                                type="text"
+                                name="name"
+                                placeholder="Biggest arms in town"
+                              />
+                            </div>
+                            <div className="mb-6">
+                              <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="category"
+                              >
+                                Category
+                              </label>
+                              <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="category"
+                                type="text"
+                                name="category"
+                                placeholder="Upper body"
+                              />
+                            </div>
                             <div className="mb-6">
                               <label
                                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -88,71 +138,52 @@ export default function ProgramsModal() {
                               </label>
                               <select
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="select-workout"
+                                id="workout"
                                 type="select"
                                 name="workout"
                                 placeholder="Biceps"
                               >
-                                <option value="Workout">
+                                <option value="workout">
                                   Choose workout to program
                                 </option>
-                                {dummyData.map((workout, index) => (
-                                  <option key={index} value={workout.name}>
-                                    {workout.name}
+                                {workouts.map((workout, index) => (
+                                  <option
+                                    key={index}
+                                    value={workout.workout_id}
+                                  >
+                                    {workout.name} ({workout.type})
                                   </option>
                                 ))}
                               </select>
                             </div>
-                            <div className="mb-6">
-                              <label
-                                className="block text-gray-700 text-sm font-bold mb-2"
-                                htmlFor="workout"
-                              >
-                                Add exercise to program
-                              </label>
-                              <select
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="select-workout"
-                                type="select"
-                                name="workout"
-                                placeholder="Push up"
-                              >
-                                <option value="Workout">
-                                  Choose exercise to program
-                                </option>
-                                {dummyData2.map((exercise, index) => (
-                                  <option key={index} value={exercise.name}>
-                                    {exercise.name}
-                                  </option>
-                                ))}
-                              </select>
-                            </div>
-                          </form>
+
+                            {/* <button type="submit">klick</button> */}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 sm:ml-3 sm:w-auto"
-                      onClick={() => setOpen(false)}
-                    >
-                      Save program
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 sm:ml-3 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                      >
+                        Save program
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
-          </div>
+          </form>
         </Dialog>
       </Transition.Root>
     </>
