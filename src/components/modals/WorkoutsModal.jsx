c7be1517f123a020e22f2cd8d00e3855651de816
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-23 11:15:56 +0100

    change bg when item is clicked in add workout

--- src/components/modals/WorkoutsModal.jsx
+++ src/components/modals/WorkoutsModal.jsx
@@ -1,10 +1,64 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useRef, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import keycloak from "../../keycloak";
 
 export default function WorkoutsModal() {
   const [open, setOpen] = useState(false);
+  const [selectedExercise, setSelectedExercise] = useState([]);
 
   const cancelButtonRef = useRef(null);
+
+  const [exercises, setExercises] = useState([]);
+  const [exercisesFetched, setExercisesFetched] = useState(false);
+
+  useEffect(() => {
+    if (!exercisesFetched) {
+      fetchExercises();
+    }
+  }, [exercisesFetched]);
+
+  const fetchExercises = () => {
+    fetch(`http://localhost:8080/api/v1/exercises`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${keycloak.token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Response not OK");
+        }
+      })
+      .then((data) => {
+        setExercises(data);
+        setExercisesFetched(true);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { name, type } = e.target.elements;
+    const workout = {
+      name: name.value,
+      type: type.value,
+    };
+    console.log(workout);
+    // addExerciseToDb(exercise);
+  }
+
+  const selectedWorkout = (index) => {
+    if (selectedExercise.includes(index)) {
+      setSelectedExercise(
+        selectedExercise.filter((exercise) => exercise !== index)
+      );
+    } else {
+      setSelectedExercise([...selectedExercise, index]);
+    }
+  };
 
   return (
     <>
@@ -34,30 +88,29 @@ export default function WorkoutsModal() {
           >
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
-
-          <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="">
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <Dialog.Title
-                          as="h3"
-                          className="text-base font-semibold leading-6 text-gray-900"
-                        >
-                          Add workout
-                        </Dialog.Title>
-                        <div className="mt-2 w-full max-w-xs">
-                          <form className="w-full">
+          <form className="w-full" onSubmit={handleSubmit}>
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div className="">
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <Dialog.Title
+                            as="h3"
+                            className="text-base font-semibold leading-6 text-gray-900"
+                          >
+                            Add workout
+                          </Dialog.Title>
+                          <div className="mt-2 w-full max-w-xs">
                             <div className="mb-4">
                               <label
                                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -87,32 +140,56 @@ export default function WorkoutsModal() {
                                 placeholder="strength"
                               />
                             </div>
-                          </form>
+                          </div>
+                        </div>
+                        <div className="pl-4 pr-4">
+                          <h5 className="block text-gray-700 text-sm font-bold mb-2">
+                            Exercise
+                          </h5>
+                          <ul className="overflow-y-scroll max-h-60 mb-2">
+                            {exercises.map((exercise, key) => (
+                              <li
+                                onClick={() => selectedWorkout(key)}
+                                key={key}
+                                className={
+                                  selectedExercise.includes(key)
+                                    ? "flex justify-between rounded-md px-1 py-1 text-sm font-medium  text-gray-700 bg-gray-900 text-white"
+                                    : "flex justify-between  text-gray-700 hover:bg-gray-700 hover:text-white rounded-md px-1 py-1 text-sm font-medium hover:cursor-pointer"
+                                }
+                                aria-current={exercise.id}
+                              >
+                                <div className="text-left w-5/6">
+                                  <p>{exercise.name}</p>
+                                  <i>Description: {exercise.description}</i>
+                                </div>
+                              </li>
+                            ))}
+                          </ul>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 sm:ml-3 sm:w-auto"
-                      onClick={() => setOpen(false)}
-                    >
-                      Save workout
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 sm:ml-3 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                      >
+                        Save workout
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
-          </div>
+          </form>
         </Dialog>
       </Transition.Root>
     </>
